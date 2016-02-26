4164ef1dde87abad7e5374a86b1db5b04cb971a4
rgupta8493 <r.gupta8493@ufl.edu>
2016-02-26 11:29:05 -0500

    VR experience built with unity 3D
    Historical VR education

---
+++ Project2VR-master/Assets/ExecutionController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExecutionController : MonoBehaviour {
+
+	public AudioClip clip;
+	public AudioSource audio;
+	public	GameObject Myself;
+	public GameObject joan;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	 void OnTriggerEnter(Collider other){
+        
+		audio.clip = clip;
+        audio.Play();
+		Myself.SetActive(false);
+		//triggerAnim.triggerAnimation ();
+		joan.GetComponent<Animator>().enabled=true;
+
+    }
+}
---
+++ Project2VR-master/Assets/MIlitaryController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MIlitaryController : MonoBehaviour {
+
+	public AudioClip clip;
+	public AudioSource audio;
+	public GameObject Myself;
+	public GameObject Myself2;
+	public GameObject Myself3;
+	public GameObject obj1;
+	public GameObject obj2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	 void OnTriggerEnter(Collider other){
+        
+		audio.clip = clip;
+        audio.Play();
+		obj1.GetComponent<AudioSource> ().enabled = true;
+		
+		Myself2.SetActive(false);
+		Myself3.SetActive(false);
+		Myself.SetActive(false);
+		//obj1.GetComponent<Animator>().enabled=false;
+		obj1.GetComponent<Animator>().enabled=true;
+		obj2.GetComponent<Animator>().enabled=true;
+    }
+}
